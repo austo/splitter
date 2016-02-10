7434801e92e5c1ad18dc56af3fa27377555db084
Austin Moran <austin.moran@surveysampling.com>
2016-02-10 08:19:53 -0500

    wrap for browser

--- index.js
+++ index.js
@@ -1,109 +1,110 @@
 'use strict';
 
-const types = {
-  INTEGER: /^-?[1-9][0-9]*$/,
-  ENUMERATION: null,
-  BOOLEAN: /^(?:(?:true)|(?:false)|0|1)$/,
-  FLOAT: /^-?[1-9][0-9]*(?:.[0-9]*)?$/,
-  DATE: /^[1-2][0-9]{3}[\.-][0-9]{2}[\.-][0-9]{2}\.?$/,
-  STRING: /.*/,
-  MULTI_ENUM: null,
-  WIDESTRING: /.*/,
-  DATE_AND_TIME: /^[1-2][0-9]{3}[\.-][0-9]{2}[\.-][0-9]{2}\.?\s(?:[0-9]{2}:){2}(?:[0-9]{2})?$/
-};
+module.exports = (function() {
 
-function Splitter(type, enums) {
-  if (!(this instanceof Splitter)) {
-    return new Splitter(enums);
-  }
-
-  type = type.toUpperCase();
-
-  if (types[type] === undefined) {
-    throw new RangeError('invalid type');
-  }
-
-  if (type === 'ENUMERATION' || type === 'MULTI_ENUM') {
-    if (!Array.isArray(enums)) {
-      throw new RangeError('ENUMERATION type splitter must be initialized with enums array');
+  function Splitter(type, enums) {
+    if (!(this instanceof Splitter)) {
+      return new Splitter(enums);
     }
-    this._rawEnums = enums;
-    this._makeMaps();
-    this.isEnum = true;
-  }
-  else {
-    this.isEnum = false;
-  }
 
-  this.type = type;
-}
+    type = type.toUpperCase();
 
-Splitter.prototype._makeMaps = function() {
-  this._enumNames = mapify(this._rawEnums, 'value', 'name');
-  this._enumIds = mapify(this._rawEnums, 'name', 'value');
-};
+    if (types[type] === undefined) {
+      throw new RangeError('invalid type');
+    }
 
-Splitter.prototype.validate = function(input, delimiters) {
-  // var self = this;
-  var re = this._makeRegExp(delimiters),
-    valid = 0,
-    invalid = 0,
-    parts = input.split(re);
-
-  // console.log(parts);
-  parts.forEach((s) => {
-    if (this.isEnum) {
-      var map = isNaN(s) ? this._enumIds : this._enumNames;
-      if (map[s] === undefined) {
-        ++invalid;
+    if (type === 'ENUMERATION' || type === 'MULTI_ENUM') {
+      if (!Array.isArray(enums)) {
+        throw new RangeError('ENUMERATION type splitter must be initialized with enums array');
       }
-      else {
-        ++valid;
-      }
+      this._rawEnums = enums;
+      this._enumNames = mapify(this._rawEnums, 'value', 'name');
+      this._enumIds = mapify(this._rawEnums, 'name', 'value');
+      this.isEnum = true;
     }
     else {
-      if (types[this.type].test(s)) {
-        ++valid;
+      this.isEnum = false;
+    }
+
+    this.type = type;
+  }
+
+  Splitter.prototype = {
+    validate: validate
+  };
+
+  const types = {
+    INTEGER: /^-?[1-9][0-9]*$/,
+    ENUMERATION: null,
+    BOOLEAN: /^(?:(?:true)|(?:false)|0|1)$/,
+    FLOAT: /^-?[1-9][0-9]*(?:.[0-9]*)?$/,
+    DATE: /^[1-2][0-9]{3}[\.-][0-9]{2}[\.-][0-9]{2}\.?$/,
+    STRING: /.*/,
+    MULTI_ENUM: null,
+    WIDESTRING: /.*/,
+    DATE_AND_TIME: /^[1-2][0-9]{3}[\.-][0-9]{2}[\.-][0-9]{2}\.?\s(?:[0-9]{2}:){2}(?:[0-9]{2})?$/
+  };
+
+  function mapify(arr, keyname, valname) {
+    var map = {};
+    arr.forEach(function(elem) {
+      map[elem[keyname]] = elem[valname];
+    });
+    return map;
+  }
+
+  function validate(input, delimiters) {
+    var re = makeRegExp(delimiters),
+      valid = 0,
+      invalid = 0,
+      parts = input.split(re);
+
+    // console.log(parts);
+    parts.forEach((s) => {
+      if (this.isEnum) {
+        var map = isNaN(s) ? this._enumIds : this._enumNames;
+        if (map[s] === undefined) {
+          ++invalid;
+        }
+        else {
+          ++valid;
+        }
       }
       else {
-        ++invalid;
+        if (types[this.type].test(s)) {
+          ++valid;
+        }
+        else {
+          ++invalid;
+        }
       }
-    }
-  });
-  return {
-    valid: valid,
-    invalid: invalid
+    });
+    return {
+      valid: valid,
+      invalid: invalid
+    };
+  }
+
+  const delimiters = {
+    COMMA: ',',
+    SEMICOLON: ';',
+    SPACE: '\\u0020+',
+    TABULATOR: '\\t',
+    NEWLINE: '\\r?\\n+'
   };
-};
 
-
-const delimiters = {
-  COMMA: ',',
-  SEMICOLON: ';',
-  SPACE: '\\u0020+',
-  TABULATOR: '\\t',
-  NEWLINE: '\\r?\\n+'
-};
-
-Splitter.prototype._makeRegExp = function(delims) {
-  var str = '';
-  delims.forEach(function(type, index) {
-    if (delimiters[type]) {
-      if (index > 0) {
-        str += '|';
+  function makeRegExp(delims) {
+    var str = '';
+    delims.forEach(function(type, index) {
+      if (delimiters[type]) {
+        if (index > 0) {
+          str += '|';
+        }
+        str += delimiters[type];
       }
-      str += delimiters[type];
-    }
-  });
-  return new RegExp(str, 'm');
-};
+    });
+    return new RegExp(str, 'm');
+  }
 
-function mapify(arr, keyname, valname) {
-  var map = {};
-  arr.forEach(function(elem) {
-    map[elem[keyname]] = elem[valname];
-  });
-  return map;
-}
-
-module.exports = Splitter;
+  return Splitter;
+}());
--- test/splitter_test.js
+++ test/splitter_test.js
@@ -2,6 +2,8 @@
 /* global test:true */
 /* global before:true */
 
+// mocha --opts test/mocha.opts -g prototype test/splitter_test.js
+
 'use strict';
 
 var path = require('path'),
@@ -42,13 +44,6 @@ suite('splitter', () => {
     assert.equal(enums.length, Object.keys(splitter._enumIds).length);
   });
 
-  test('should make regex correctly', () => {
-    var splitter = new Splitter('INTEGER');
-    var re = splitter._makeRegExp(['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
-    var parts = 'this,should;provide every\ttype\nof\r\ndelimiter'.split(re);
-    assert.equal(7, parts.length);
-  });
-
   test('should validate enums correctly', () => {
     var splitter = new Splitter('enumeration', enums);
     // inspect(splitter._enumIds);
@@ -62,8 +57,7 @@ suite('splitter', () => {
   test('should validate enums correctly with extra spaces', () => {
     var splitter = new Splitter('enumeration', enums);
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`ACCOMACK, Virginia\tADA, Idaho\t17001\tdog`,
-      ['NEWLINE', 'TABULATOR']);
+    var result = splitter.validate(`ACCOMACK, Virginia\tADA, Idaho\t17001\tdog`, ['NEWLINE', 'TABULATOR']);
     assert.deepEqual({
       valid: 3,
       invalid: 1
@@ -73,8 +67,7 @@ suite('splitter', () => {
   test('should validate integer with all delimiters', () => {
     var splitter = new Splitter('integer');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`1,2;3 4\t5\r\n6`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`1,2;3 4\t5\r\n6`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 6,
       invalid: 0
@@ -84,8 +77,7 @@ suite('splitter', () => {
   test('should validate boolean with all delimiters', () => {
     var splitter = new Splitter('boolean');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`true,false;true false\t1\r\n0`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`true,false;true false\t1\r\n0`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 6,
       invalid: 0
@@ -95,8 +87,7 @@ suite('splitter', () => {
   test('should validate float with all delimiters', () => {
     var splitter = new Splitter('float');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`1.1,2.2;3.3 4.4\t5.5\r\n6.6`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`1.1,2.2;3.3 4.4\t5.5\r\n6.6`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 6,
       invalid: 0
@@ -106,8 +97,7 @@ suite('splitter', () => {
   test('should validate date with all delimiters', () => {
     var splitter = new Splitter('date');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`1999-12-25,1999-12-25;1999-12-25 1999-12-25\t1999-12-25\r\n1999-12-25`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`1999-12-25,1999-12-25;1999-12-25 1999-12-25\t1999-12-25\r\n1999-12-25`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 6,
       invalid: 0
@@ -117,8 +107,7 @@ suite('splitter', () => {
   test('should validate date with all delimiters', () => {
     var splitter = new Splitter('date');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`1999-12-25,1999-12-25;1999-12-25 1999-12-25\t1999-12-25\r\n1999-12-25`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`1999-12-25,1999-12-25;1999-12-25 1999-12-25\t1999-12-25\r\n1999-12-25`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 6,
       invalid: 0
@@ -128,8 +117,7 @@ suite('splitter', () => {
   test('should validate string with all delimiters', () => {
     var splitter = new Splitter('string');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`this,should;provide every\ttype\nof\r\ndelimiter`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`this,should;provide every\ttype\nof\r\ndelimiter`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 7,
       invalid: 0
@@ -149,8 +137,7 @@ suite('splitter', () => {
   test('should validate widestring with all delimiters', () => {
     var splitter = new Splitter('widestring');
     // inspect(splitter._enumIds);
-    var result = splitter.validate(`this,should;provide every\ttype\nof\r\ndelimiter`,
-      ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
+    var result = splitter.validate(`this,should;provide every\ttype\nof\r\ndelimiter`, ['COMMA', 'SEMICOLON', 'SPACE', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 7,
       invalid: 0
@@ -161,8 +148,7 @@ suite('splitter', () => {
     var splitter = new Splitter('DATE_AND_TIME');
     // inspect(splitter._enumIds);
     var result = splitter.validate('1999-12-25 12:25:25,1999-12-26 12:25:25;1999-12-27 12:25:25' +
-      '\t1999-12-28 12:25:25\r\n1999-12-29 12:25:25',
-      ['COMMA', 'SEMICOLON', 'TABULATOR', 'NEWLINE']);
+      '\t1999-12-28 12:25:25\r\n1999-12-29 12:25:25', ['COMMA', 'SEMICOLON', 'TABULATOR', 'NEWLINE']);
     assert.deepEqual({
       valid: 5,
       invalid: 0
